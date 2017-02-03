3b5f4a0e6302bad8e345b85258bcd3eec6b3b9aa
kinkrarin <kinkrarin@gmail.com>
2017-02-03 13:12:47 +0200

    ticket:3 view that rander page

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -5,3 +5,12 @@ from apps.hello.models import Bio
 def home(request):
     aboutme = Bio.objects.first()
     return render(request, 'main.html', {'aboutme': aboutme})
+
+
+def request_list(request):
+    req_records = Requests.objects.all().order_by('-req_time')[:10]
+    if request.is_ajax():
+        req_records = serializers.serialize("json", req_records)
+        return HttpResponse(req_records,  content_type="application/json")
+    else:
+        return render(request, 'requests.html', {'req_records': req_records})
