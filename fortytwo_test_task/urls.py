2e69d379ff6d6f623349f430bf2b1708cb04bcdb
kinkrarin <kinkrarin@gmail.com>
2017-02-03 13:43:31 +0200

    ticket:3 added urls

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -10,4 +10,8 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^request_list/$', views.request_list, name='request_list'),
+    url(r'^request_list_ajax/$',
+        views.request_list_ajax,
+        name='request_list_ajax'),
 )
