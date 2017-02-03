0d09c86b3101b5294f88b5de97bc2eb25df07072
kinkrarin <kinkrarin@gmail.com>
2017-02-03 13:14:18 +0200

    ticket:3 test for middleware

---
+++ apps/hello/tests/test_middleware.py
@@ -0,0 +1,19 @@
+from django.test import TestCase, Client
+from apps.hello.models import Bio, Requests
+
+
+
+class TestMiddleware(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_count_entries(self):
+        """ test middleware correctly saving count of objects """
+        req = Requests.objects.count()
+        self.assertEqual(req, 0)
+        self.client.get('request_list')
+        req = Requests.objects.all()
+        self.assertEqual(req.count(), 1)
+        self.assertEqual(req.last().path, 'http://testserver/request_list')
+        self.assertEqual(req.last().method, 'GET')
+        
